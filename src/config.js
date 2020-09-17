2fa68b11e153a454137d5ec0fa2f8c9273f6e9a0
Kevin Park <keunsuk413@gmail.com>
2020-09-17 11:43:47 -0500

    modified API_ENDPOINT

--- src/config.js
+++ src/config.js
@@ -1,5 +1,4 @@
 export default {
-  API_ENDPOINT:
-    "postgres://ylkkrnwfxpgowp:a4fc586f4f35f7713991afed0052a3f42a175ba3151aa163e882b223b2765557@ec2-54-211-169-227.compute-1.amazonaws.com:5432/d8eg7rg288j4fd",
+  API_ENDPOINT: "https://winngingeleven.herokuapp.com/api",
   TOKEN_KEY: "we-client-auth-token",
 };
